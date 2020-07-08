f2928f8ff5e000b375d9cf58d9acfa6951483a1d
David Morton <55235981+davidmorton0@users.noreply.github.com>
2020-07-08 20:18:55 +0000

    basic info page

--- app.rb
+++ app.rb
@@ -1,11 +1,15 @@
 require 'sinatra'
 require 'sinatra/activerecord'
+require 'pry'
 
 set :database, "sqlite3:project-name.sqlite3"
 
 require './models'
 
 get '/' do
-  @users = User.all
-  erb :test
+  @person = "Bob"
+  @theme = "Coding"
+  @persons = ["Alice", "Bob", "Catherine", "Daniel"]
+  @themes = ["Variables", "Methods", "Errors", "Constants", "Coding"]
+  erb :index
 end
